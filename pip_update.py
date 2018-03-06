aa01e4a1de49272a46c2322156ce07dea7c8160b
Hyeongsu Kim <883w@users.noreply.github.com>
2018-03-06 16:01:59 +0900

    Create pip_update.py

---
+++ pip_update.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import pip
+from subprocess import call
+
+packages = [dist.project_name for dist in pip.get_installed_distributions()]
+call("pip install --upgrade " + ' '.join(packages), shell=True)

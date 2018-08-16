4746c669f678701214bf3df573702aeaaf7024ac
Karlos Kim <4891802+800G@users.noreply.github.com>
2018-08-16 17:37:35 +0900

    Update pip_update.py
    pip 18.0 버전 대응

--- pip_update.py
+++ pip_update.py
@@ -1,8 +1,8 @@
-#!/usr/local/bin/python
+#/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pip
+from pip._internal.utils.misc import get_installed_distributions
 from subprocess import call
 
-packages = [dist.project_name for dist in pip.get_installed_distributions()]
+packages = [dist.project_name for dist in get_installed_distributions()]
 call("pip install --upgrade " + ' '.join(packages), shell=True)
